53ab9d1346e796c5ec7914d73300b2389e6467a0
DmKorshenkov <amud101178@mail.ru>
2024-12-12 16:21:44 +0300

    main

--- main.go
+++ main.go
@@ -1,11 +1,5 @@
-package main
+package main 
 
 func main(){
-
-	print("branch no_master")
-	print("branch no_master2")
-
-	print("branch master")
-	print("branch master2")
-	
+	print("branch main")
 }
